0fc6e62ba5d328f152450e736840c71b2024e10e
AlunanAndrea <75357994+AlunanAndrea@users.noreply.github.com>
2021-01-04 12:45:36 +0800

    Add files via upload

---
+++ canvas/7.1/canvas.js
@@ -0,0 +1,336 @@
+var canvas = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.moveTo(15, 46);
+ctx.lineTo(187, 46);
+ctx.lineTo(187, 156);
+ctx.lineTo(15, 156);
+ctx.lineTo(15, 46);
+ctx.closePath();
+ctx.fillStyle = "#FF0000";
+ctx.fill();	
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(143, 100);
+ctx.lineTo(78, 140);
+ctx.lineTo(78, 62);
+ctx.lineTo(143, 100);
+ctx.closePath();
+ctx.fillStyle = "#FFF";
+ctx.fill();	
+ctx.stroke();
+
+var canvas = document.getElementById("canvas2");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(103, 15);
+ctx.lineTo(196, 15);
+ctx.lineTo(196, 171);
+ctx.lineTo(103, 171);
+ctx.lineTo(103, 15);
+ctx.closePath();
+ctx.fillStyle = "#000";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(3, 15);
+ctx.lineTo(96, 15);
+ctx.lineTo(96, 171);
+ctx.lineTo(3, 171);
+ctx.lineTo(3, 15);
+ctx.closePath();
+ctx.fillStyle = "#000";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(159, 25);
+ctx.lineTo(164, 25);
+ctx.lineTo(164, 45);
+ctx.lineTo(159, 45);
+ctx.lineTo(159, 25);
+ctx.closePath();
+ctx.fillStyle = "#F2F2F2";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(142, 22);
+ctx.lineTo(157, 22);
+ctx.lineTo(157, 88);
+ctx.lineTo(142, 88);
+ctx.lineTo(142, 22);
+ctx.closePath();
+ctx.fillStyle = "#0C00FF";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(31, 160);
+ctx.lineTo(69, 160);
+ctx.lineTo(69, 168);
+ctx.lineTo(31, 168);
+ctx.lineTo(31, 160);
+ctx.closePath();
+ctx.fillStyle = "#0C00FF";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(6, 18);
+ctx.lineTo(93, 18);
+ctx.lineTo(93, 156);
+ctx.lineTo(6 ,156);
+ctx.lineTo(6, 18);
+ctx.closePath();
+ctx.fillStyle = "#93CDD7";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(143, 25);
+ctx.lineTo(156, 25);
+ctx.lineTo(156, 59);
+ctx.lineTo(143, 59);
+ctx.lineTo(143, 25);
+ctx.closePath();
+ctx.fillStyle = "#F2F2F2";
+ctx.fill();
+ctx.stroke();
+
+
+var canvas = document.getElementById("canvas3");
+var ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.moveTo(125, 125);
+ctx.lineTo(140, 140);
+ctx.lineTo(62, 140);
+ctx.lineTo(78, 125);
+ctx.lineTo(125, 125);
+ctx.closePath();
+ctx.fillStyle = "#777780";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(15, 15);
+ctx.lineTo(187, 15);
+ctx.lineTo(187, 125);
+ctx.lineTo(15, 125);
+ctx.lineTo(15, 15);
+ctx.closePath();
+ctx.fillStyle = "#777780";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(21, 21);
+ctx.lineTo(181, 21);
+ctx.lineTo(181, 118);
+ctx.lineTo(21, 118);
+ctx.lineTo(21, 21);
+ctx.closePath();
+ctx.fillStyle = "#93CDD7";
+ctx.fill();
+ctx.stroke();
+
+var canvas = document.getElementById("canvas4");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(78, 31);
+ctx.lineTo(93, 31);
+ctx.lineTo(93, 156);
+ctx.lineTo(78, 156);
+ctx.lineTo(78, 31);
+ctx.closePath();
+ctx.fillStyle = "#B9B5B5";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(87, 18);
+ctx.lineTo(86, 12);
+ctx.lineTo(84, 18);
+ctx.lineTo(87, 18);
+ctx.closePath();
+ctx.fillStyle = "#000";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(93, 31);
+ctx.lineTo(87, 18);
+ctx.lineTo(84, 18);
+ctx.lineTo(78, 31);
+ctx.lineTo(93, 31);
+ctx.closePath();
+ctx.fillStyle = "#0962F4";
+ctx.fill();
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(78, 162);
+ctx.lineTo(93, 162);
+ctx.lineTo(93, 175);
+ctx.lineTo(78, 175);
+ctx.lineTo(78, 162);
+ctx.closePath();
+ctx.fillStyle = "#0962F4";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(78, 156);
+ctx.lineTo(93, 156);
+ctx.lineTo(93, 162);
+ctx.lineTo(78, 162);
+ctx.lineTo(78, 156);
+ctx.closePath();
+ctx.fillStyle = "#000";
+ctx.fill();
+ctx.stroke();
+
+
+
+var canvas = document.getElementById("canvas5");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(46, 109);
+ctx.lineTo(140, 109);
+ctx.lineTo(140, 187);
+ctx.lineTo(46, 187);
+ctx.lineTo(46, 109);
+ctx.closePath();
+ctx.fillStyle = "#D1D1D7";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(50, 78);
+ctx.lineTo(137, 78);
+ctx.lineTo(137, 109);
+ctx.lineTo(50, 109);
+ctx.lineTo(50, 78);
+ctx.closePath();
+ctx.fillStyle = "#D1D1D7";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(65, 78);
+ctx.lineTo(75, 78);
+ctx.lineTo(75, 15);
+ctx.lineTo(65, 15);
+ctx.lineTo(65, 78);
+ctx.closePath();
+ctx.fillStyle = "#FFF";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(112, 78);
+ctx.lineTo(121, 78);
+ctx.lineTo(121, 15);
+ctx.lineTo(112, 15);
+ctx.lineTo(112, 78);
+ctx.closePath();
+ctx.fillStyle = "#FFF";
+ctx.fill();
+ctx.stroke();
+
+var canvas = document.getElementById("canvas6");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(46, 15);
+ctx.lineTo(140, 15);
+ctx.lineTo(140, 109);
+ctx.lineTo(46, 109);
+ctx.lineTo(46, 15);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(53, 21);
+ctx.lineTo(134, 21);
+ctx.lineTo(134, 102);
+ctx.lineTo(53, 102);
+ctx.lineTo(53, 21);
+ctx.closePath();
+ctx.fillStyle = "#FFF";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(90, 109);
+ctx.lineTo(96, 109);
+ctx.lineTo(96, 193);
+ctx.lineTo(90, 193);
+ctx.lineTo(90, 109);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+
+var canvas = document.getElementById("canvas7");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(81, 28);
+ctx.lineTo(90, 28);
+ctx.lineTo(90, 187);
+ctx.lineTo(81, 187);
+ctx.lineTo(81, 28);
+ctx.closePath();
+ctx.fillStyle = "#543C34";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(46, 31);
+ctx.lineTo(125, 31);
+ctx.lineTo(125, 62);
+ctx.lineTo(46, 62);
+ctx.lineTo(46, 31);
+ctx.closePath();
+ctx.fillStyle = "#4C4B4A";
+ctx.fill();
+ctx.stroke();
+
+var canvas = document.getElementById("canvas8");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(93, 15);
+ctx.lineTo(109, 15);
+ctx.lineTo(109, 187);
+ctx.lineTo(93, 187);
+ctx.lineTo(93, 15);
+ctx.closePath();
+ctx.fillStyle = "#543C34";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(46, 62);
+ctx.lineTo(156, 62);
+ctx.lineTo(156, 78);
+ctx.lineTo(46, 78);
+ctx.lineTo(46, 62);
+ctx.closePath();
+ctx.fillStyle = "#543C34";
+ctx.fill();
+ctx.stroke();
